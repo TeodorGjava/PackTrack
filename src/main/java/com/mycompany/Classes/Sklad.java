384ce5ccb446d6f391d60f0876f51f45b336880f
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-03-26 18:36:05 +0200

    Push after a few months working on it
    Some raw but working, in fact already in use at my current job.

---
+++ src/main/java/com/mycompany/Classes/Sklad.java
@@ -0,0 +1,45 @@
+package com.mycompany.Classes;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LL
+ */
+class Sklad {
+     private String IDopakovka;
+    private String Status;
+    private String Location;
+    private String datestamp;
+   
+    private String numWh;
+    public Sklad(String IDopakovka,String Status,String Location,String datestamp,  String numWh){
+    this.IDopakovka = IDopakovka;
+    this.Status = Status;
+    this.datestamp = datestamp;
+    this.Location = Location;
+    
+    this.numWh=numWh;
+    }
+    public String getID(){
+    return IDopakovka;
+    }
+    public String getStatus(){
+    return Status;
+    }
+    public String getLocation(){
+    return Location;
+    }
+    public String getdatestamp(){
+    return datestamp;
+    }
+    
+    public String getNumWh(){
+    return numWh;
+    }
+}
